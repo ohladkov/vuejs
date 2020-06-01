e5a883d36d4e08bc06bd2c146a6cb33a9da8c508
jc_ohladkov <ohladkov@justcoded.co>
2020-06-01 18:53:34 +0300

    setup project

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <div id="app">
+    Vue JS mounted successfully
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'App',
+};
+</script>
+
+<style lang="scss">
+
+</style>
